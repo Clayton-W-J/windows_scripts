2de232467be7f0641f88e019fb57ae8e7d408bd8
Clayton-W-J <106715412+Clayton-W-J@users.noreply.github.com>
2024-05-28 16:54:16 -0500

    Update chkdsk.bat

--- chkdsk_task/chkdsk.bat
+++ chkdsk_task/chkdsk.bat
@@ -1,2 +1,6 @@
+#automatically respond yesto any prompts from the chkdsk command. Which will check for and (/r)epair disk errors and (/f)ix them.
+#Shuts down the computer immediately, forcing running applications to close, and it will not allow users to abort the shutdown. Delay before shutdown is set to 0 seconds, 
+#ensuring the shutdown occurs immediately.
+
 echo y^| chkdsk C: /r /f
 shutdown /s /f /t 0
