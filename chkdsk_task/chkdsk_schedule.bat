4cf5527d84f33c06eaecb77a6f9780006a985976
Clayton-W-J <106715412+Clayton-W-J@users.noreply.github.com>
2024-05-28 16:49:58 -0500

    Update chkdsk_schedule.bat

--- chkdsk_task/chkdsk_schedule.bat
+++ chkdsk_task/chkdsk_schedule.bat
@@ -1,1 +1,4 @@
+#So, the command creates a new scheduled task named "RunChkdskAndShutdown" that will run a batch file located at "path\to\.bat\" once, starting at 5:00 PM, with the highest privileges. 
+#If a task with the same name already exists, it will be overwritten without prompting for confirmation.
+
 schtasks /create /tn "RunChkdskAndShutdown" /tr "path\to\.bat\" /sc once /st 17:00 /rl highest /f
