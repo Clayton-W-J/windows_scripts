e25faa63434f2cb63c7e4ee2818bb9d9b04ea878
CJ <106715412+Clayton-W-J@users.noreply.github.com>
2024-06-14 08:49:46 -0500

    Update chkdsk_schedule.bat

--- chkdsk_task/chkdsk_schedule.bat
+++ chkdsk_task/chkdsk_schedule.bat
@@ -1,4 +1,6 @@
 # Creates a new scheduled task named "RunChkdskAndRestart" that will run a batch file located at "path\to\.bat\" once, at the desired time, and with the highest privileges. 
 # If a task with the same name already exists, it will be overwritten without prompting for confirmation.
 
+set-executionpolicy bypass
 schtasks /create /tn "RunChkdskAndRestart" /tr "path\to\.bat\" /sc once /st 17:00 /rl highest /f
+schtasks /query /tn "RunChkdskAndRestart"
